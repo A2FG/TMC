1dc77183d48f57fa086b99782d53e03c92c4bced
A2FG <abcalic11@gmail.com>
2025-01-09 21:42:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 
 const copyButton = document.getElementById('copy-ip-btn');
-const serverIP = "TitanMC.aternos.me";  
+const serverIP = "TitanMC.aternos.me";
 
 function copyIP() {
     navigator.clipboard.writeText(serverIP)
         .then(() => {
-           
             copyButton.classList.add('copied');
-            
-           
             alert("تم نسخ الـ IP بنجاح!");
-
-           
             setTimeout(() => {
                 copyButton.classList.remove('copied');
-            }, 500);  
+            }, 500);
         })
         .catch(err => {
             alert("حدث خطأ أثناء نسخ الـ IP.");
             console.error('Error copying text: ', err);
         });
 }
-
 
 copyButton.addEventListener('click', copyIP);
 
@@ -42,4 +36,40 @@ const observer = new IntersectionObserver((entries, observer) => {
 
 sections.forEach(section => {
   observer.observe(section);
+});
+
+
+
+function fetchServerStatus() {
+  fetch('https://discordsrv.api/v1/status')
+      .then(response => response.json())
+      .then(data => {
+          const statusText = document.getElementById('server-status-text');
+          const statusImage = document.getElementById('server-status-image');
+          
+          if (data.status === 'online') {
+              statusText.textContent = 'أونلاين';
+              statusImage.src = 'photo/icons/server-online.png'; 
+          } else {
+              statusText.textContent = 'غير متصل';
+              statusImage.src = 'photo/icons/server-offline.png'; 
+          }
+      })
+      .catch(err => {
+          console.error('Error fetching server status: ', err);
+          document.getElementById('server-status-text').textContent = 'خطأ في تحميل البيانات';
+          document.getElementById('server-status-image').src = 'photo/icons/server-error.png'; // صورة الخطأ
+      });
+}
+
+document.addEventListener('DOMContentLoaded', fetchServerStatus);
+
+
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  console.log("الزر تم الضغط عليه");
+  navLinks.classList.toggle('active');
 });
